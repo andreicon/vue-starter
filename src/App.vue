6859b81e7595512e22b29bdb6c73812a54a8a337
Andrei Costea <andrei@mindfullab.co>
2020-07-14 16:29:58 +0300

    try out a ts component

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,35 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue, { VNode } from 'vue'
+
+const Component = Vue.extend({
+  data () {
+    return {
+      msg: 'Hello'
+    }
+  },
+  methods: {
+    // need annotation due to `this` in return type
+    greet (): string {
+      return this.msg + ' world'
+    }
+  },
+  computed: {
+    // need annotation
+    greeting(): string {
+      return this.greet() + '!'
+    }
+  },
+  // `createElement` is inferred, but `render` needs return type
+  render (createElement): VNode {
+    return createElement('div', this.greeting)
+  }
+})
+export default Component
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
